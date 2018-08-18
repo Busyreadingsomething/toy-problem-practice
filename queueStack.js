e5b585ab250c361512e12c7a2e0f3c61f30f0484
Makan Azarshahy <oro1919@gmail.com>
2018-08-17 18:39:50 -0700

    Implemented Passing 100% solution

--- queueStack.js
+++ queueStack.js
@@ -1,30 +1,66 @@
-class QueueStack {
+// class QueueStack {
+//   constructor() {
+//     this.enStack = [];
+//     this.deStack = [];
+//   }
+
+//   enqueue(val) {
+//     const len = this.deStack.length;
+//     if (len > 0) {
+//       for (let i = 0; i < len; i += 1) {
+//         this.enStack.push(this.deStack.pop());
+//       }
+//     }
+//     this.enStack.push(val);
+//   }
+
+//   dequeue() {
+//     const len = this.enStack.length;
+//     let val;
+//     if (len > 0) {
+//       for (let i = 0; i < len; i += 1) {
+//         this.deStack.push(this.enStack.pop());
+//       }
+//     }
+//     val = this.deStack.pop();
+//     return val;
+//   }
+// }
+
+// Implemented a better Queue Stack 100% and Passing
+class MyQueue {
   constructor() {
-    this.enStack = [];
-    this.deStack = [];
+    this.front = [];
+    this.back = [];
   }
 
-  enqueue(val) {
-    const len = this.deStack.length;
-    if (len > 0) {
-      for (let i = 0; i < len; i += 1) {
-        this.enStack.push(this.deStack.pop());
-      }
+  push(x) {
+    const len = this.front.length;
+    for (let i = 0; i < len; i += 1) {
+      this.back.push(this.front.pop());
     }
-    this.enStack.push(val);
+    
+    this.back.push(x);
   }
 
-  dequeue() {
-    const len = this.enStack.length;
-    let val;
-    if (len > 0) {
-      for (let i = 0; i < len; i += 1) {
-        this.deStack.push(this.enStack.pop());
-      }
+  pop() {
+    const len = this.back.length;
+    for (let i = 0; i < len; i += 1) {
+      this.front.push(this.back.pop());
     }
-    val = this.deStack.pop();
-    return val;
+    
+    return this.front.pop();
   }
+  peek() {
+    if (this.back.length > 0) {
+      return this.back[0];
+    }
+    return this.front[this.front.length - 1];
+  }
+  
+  empty() {
+    return this.back.length === 0 && this.front.length === 0;
+  };
 }
 
 const test = new QueueStack();
