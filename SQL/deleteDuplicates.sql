99ffb587a21e9729e34a16988fed765972ff7275
Makan Azarshahy <oro1919@gmail.com>
2018-09-03 15:39:01 -0700

    Added deleteDuplicates query

---
+++ SQL/deleteDuplicates.sql
@@ -0,0 +1,16 @@
+DELETE FROM
+  Person
+WHERE
+  Id NOT IN (
+    SELECT
+      Id
+    FROM
+      (
+        SELECT
+          MIN(Id) AS Id
+        FROM
+          Person
+        GROUP BY
+          Email
+      ) p
+  );
