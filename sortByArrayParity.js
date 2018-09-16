a04ca8b8082419430777bd63fe0741ea7b805798
Makan Azarshahy <oro1919@gmail.com>
2018-09-16 12:26:14 -0700

    Added Original attempt with linear space and linear time (2n)

--- sortByArrayParity.js
+++ sortByArrayParity.js
@@ -1,3 +1,19 @@
+// O(n) Space and O(2n) Time
+var sortArrayByParity = function(A) {
+  const even = [];
+  const odd = [];
+  
+  for (const num of A) {
+    if (num % 2 === 0) {
+      even.push(num);
+    } else {
+      odd.push(num);
+    }
+  }
+  
+  return [...even, ...odd];
+};
+
 // O(n) Time && O(1) Space
 var sortArrayByParity = function(A) {
   let start = 0;
